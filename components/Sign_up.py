cb1fe69db07516a754b6899210567bd0a3e03a0c
TwitterN0tifcationBot <164622308+TwitterN0tifcationBot@users.noreply.github.com>
2025-03-04 21:19:34 +0100

    Create Sign_up.py

---
+++ components/Sign_up.py
@@ -0,0 +1,16 @@
+import name = "Sign up"
+    add.new(frame)
+frame.color(grey)
+frame.name("SignupFrame")
+SignupFrame.transparency = 0.3
+Signup.location = Middle
+Signup.size(864, 642)
+
+ add.new(text)
+text.name  = "Title"
+Title.color = blue
+title.text = "Create your account."
+
+add.new(textbox)
+textbox.name = "ACC_NAME"
+ACC_name.placeholder = "Your new account name."
