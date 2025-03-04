08a95b540da20e57d8d152f8646ac687aeb1c98a
TwitterN0tifcationBot <164622308+TwitterN0tifcationBot@users.noreply.github.com>
2025-03-04 21:25:42 +0100

    Update Sign_up.py

--- components/Sign_up.py
+++ components/Sign_up.py
@@ -14,3 +14,30 @@ title.text = "Create your account."
 add.new(textbox)
 textbox.name = "ACC_NAME"
 ACC_name.placeholder = "Your new account name."
+ACC_NAME.color = white
+required(True)
+
+add.new(textbox)
+textbox.name = "ACC_PASS"
+ACC_PASS.placeholder = "Your account's password."
+ACC_PASS.color = white
+required(True)
+
+
+add.new(button)
+button.name = "CREATE_ACC"
+CREATE_ACC.text = "Create Account"
+if CREATE_ACC.pressed = True
+new.database(
+    name=ACC_NAME.input
+    password=ACC_PASS.input
+)
+
+if ACC_NAME.used = True
+add.new(text="Account name is used!")
+if ACC_PASS = "Password", "1234"
+add.new(text)
+text.name = "ErrorHandeler"
+ErrorHandeler.text = "Password is to plain."
+wait(10)
+destroy(ErrorHandeler)
