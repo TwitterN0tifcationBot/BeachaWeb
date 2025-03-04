20a94b7d158e1bcde5aef75ea1b78a34268f6f7d
TwitterN0tifcationBot <164622308+TwitterN0tifcationBot@users.noreply.github.com>
2025-03-04 21:28:37 +0100

    Create 404.py

---
+++ 404.py
@@ -0,0 +1,12 @@
+import name = 404
+
+  add.new(text)
+text.name = "Title"
+Title.type = Title
+Title.color = "Green"
+
+add.new(text)
+text.name = "description"
+description.type = small_text
+description.color = "black"
+description.text = "The page you are looking for doesnt exist or access has been denied."
